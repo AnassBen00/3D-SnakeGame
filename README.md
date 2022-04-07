a571582296f94dcd2ea8dab6e4123419f80f9e23
Anass <benzekrianass@gmail.com>
2022-04-07 23:20:40 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# JavaFX Sample Maven Project
+a simple sample of the use of JavaFX with Maven
+
+## Build
+Enter the following commands:
+
+`mvn clean`
+
+`mvn compile`
+
+`mvn package`
+
+## Run
+Enter the following command from the root directory:
+
+`java -cp "target/jmonkey-1.0.0-SNAPSHOT-dist/*" fr.utln.jmonkey.HelloWorld`
+
+## Tutorials
+[https://wiki.jmonkeyengine.org/docs/3.4/tutorials/beginner/hello_simpleapplication.html](https://wiki.jmonkeyengine.org/docs/3.4/tutorials/beginner/hello_simpleapplication.html)
---
+++ src/main/java/fr/utln/jmonkey/snake.java
@@ -0,0 +1,30 @@
+package fr.utln.jmonkey;
+
+import com.jme3.asset.AssetManager;
+import com.jme3.material.Material;
+import com.jme3.math.ColorRGBA;
+import com.jme3.math.Vector3f;
+import com.jme3.scene.Geometry;
+import com.jme3.scene.Node;
+import com.jme3.scene.shape.Box;
+
+public class snake {
+    Geometry body;
+
+    snake(Node rootNode, AssetManager assetManager)
+    {
+        Box b = new Box(Vector3f.UNIT_X, 5f, 5f, 5f);
+        body = new Geometry("Box", b);
+
+        Material mat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
+        mat.setColor("Color", ColorRGBA.Blue);
+        mat.getAdditionalRenderState().setWireframe(true);
+        body.setMaterial(mat);
+        rootNode.attachChild(body);
+    }
+
+    public void snakeMove(Vector3f position)
+    {
+        body.move(position);
+    }
+}
